ee0618971c864c88e85d722759a380ace32c750a
John-wallace89 <john.wallace@heineken.co.uk>
2021-08-13 15:39:32 +0000

    Update - added Login functionality.

--- app.py
+++ app.py
@@ -46,7 +46,34 @@ def register():
         # put new user in 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Welcome to the Pack!")
+    
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check username exists
+        existing_member = mongo.db.users.find_one(
+            {'username': request.form.get("username").lower()})
+        
+        if existing_member:
+            # ensure password matched username
+            if check_password_hash(
+                    existing_member["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect (url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
