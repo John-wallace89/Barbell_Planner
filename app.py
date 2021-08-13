0752e0efee20d90d1ebae968c06366c3871f49d6
John-wallace89 <john.wallace@heineken.co.uk>
2021-08-13 14:46:34 +0000

    Update - add register functionality

--- app.py
+++ app.py
@@ -29,6 +29,23 @@ def get_workouts():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check username
+        existing_member = mongo.db.users.find_one(
+            {'username': request.form.get("username").lower()})
+
+        if existing_member:
+            flash("Username already exists!")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        # put new user in 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Welcome to the Pack!")
     return render_template("register.html")
 
 
