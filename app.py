84ed80c9991c9bbb3b96f54e6c38eb00e8bfa10b
John-wallace89 <john.wallace@heineken.co.uk>
2021-09-09 16:26:20 +0000

    Update - error handling pages added.

--- app.py
+++ app.py
@@ -162,6 +162,18 @@ def delete_workout(workout_id):
     return redirect(url_for("get_workouts"))
 
 
+# 404 error redirect
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# 500 error redirect
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
